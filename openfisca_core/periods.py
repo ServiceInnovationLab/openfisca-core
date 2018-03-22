2e61f65a7182b0871e441b59c43dff19ad1eab0e
Anna-Livia <annalivia.gomart@gmail.com>
2018-03-22 18:09:37 +0100

    Change cmp to key in the sort algorythm

--- openfisca_core/periods.py
+++ openfisca_core/periods.py
@@ -864,6 +864,32 @@ def compare_period_start(a, b):
     return (start_a > start_b) - (start_a < start_b)
 
 
+def cmp_to_key(mycmp):
+    'Convert a cmp= function into a key= function'
+    # from http://code.activestate.com/recipes/576653-convert-a-cmp-function-to-a-key-function/
+    class K(object):
+        def __init__(self, obj, *args):
+            self.obj = obj
+
+        def __lt__(self, other):
+            return mycmp(self.obj, other.obj) < 0
+
+        def __gt__(self, other):
+            return mycmp(self.obj, other.obj) > 0
+
+        def __eq__(self, other):
+            return mycmp(self.obj, other.obj) == 0
+
+        def __le__(self, other):
+            return mycmp(self.obj, other.obj) <= 0
+
+        def __ge__(self, other):
+            return mycmp(self.obj, other.obj) >= 0
+
+        def __ne__(self, other):
+            return mycmp(self.obj, other.obj) != 0
+    return K
+
 # Level-1 converters
 
 
