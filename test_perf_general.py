feece2a77f746e9d7f0b83a912e20d7175387b0b
sandcha <sandcha@users.noreply.github.com>
2018-06-01 10:35:43 +0200

    Compare calculate performances with and without trace

--- test_perf_general.py
+++ test_perf_general.py
@@ -8,16 +8,15 @@ from openfisca_core.scripts.simulation_generator import make_simulation, randoml
 from openfisca_france import CountryTaxBenefitSystem
 
 
-
 tbs = CountryTaxBenefitSystem()
 
 N = 1  # nombre familles
 P = 4 * N  # nombre personnes
-NB_S = 10
+NB_S = 100
 period = '2018-04'
 
-def init_simulation():
-  simulation = make_simulation(tbs, P, N, trace=True)
+def init_simulation(with_trace):
+  simulation = make_simulation(tbs, P, N, trace=with_trace)
   # randomly_init_variable(simulation, 'salary', '2018-04', 4000)
   randomly_init_variable(simulation, 'salaire_net', '2018-04', 4000)
   # y = simulation.household.members_position
@@ -25,21 +24,36 @@ def init_simulation():
 
   return simulation
 
-simulations = [init_simulation() for i in range(NB_S)]
+simulations_with_trace = [init_simulation(True) for i in range(NB_S)]
+simulations_without_trace = [init_simulation(False) for i in range(NB_S)]
 
 
 # test calculate
-def timed():
-  for simulation in simulations:
+def timed_without_trace():
+  for simulation in simulations_without_trace:
+    # salary_i = simulation.household.members('salary', '2018-04').sum()
+    simulation.calculate('revenu_disponible', 2018)
+
+def timed_with_trace():
+  for simulation in simulations_with_trace:
     # salary_i = simulation.household.members('salary', '2018-04').sum()
     simulation.calculate('revenu_disponible', 2018)
 
 
 # measure specific function duration
+print("Without trace - #simulations: " + str(NB_S))
 start_time = time.time()
-timed()
-dt = time.time() - start_time
-print("Total time: --- %s seconds ---" % (dt))
+timed_without_trace()
+dt_without = time.time() - start_time
+print("Total time: --- %s seconds ---" % (dt_without))
+
+print("With trace - #simulations: " + str(NB_S))
+start_time = time.time()
+timed_with_trace()
+dt_with = time.time() - start_time
+print("Total time: --- %s seconds ---" % (dt_with))
+
+print("% diff: " + str(100 * ((dt_with - dt_without)/dt_without)))
 
 # profile specific function
 # cProfile.run("timed()", 'NEW.prof')
