99943da35d02930646c1cfc9f40b22041ccc13ec
sandcha <sandcha@users.noreply.github.com>
2018-06-01 15:54:28 +0200

    Test tbs loading performance

---
+++ tests/core/performance/test_load_tax_benefit_system.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import time
+
+from openfisca_france import CountryTaxBenefitSystem
+
+
+start_time = time.time()
+tax_benefit_system = CountryTaxBenefitSystem()
+dt = time.time() - start_time
+print("Total time: --- %s seconds ---" % (dt))
